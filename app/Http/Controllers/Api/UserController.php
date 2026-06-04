39d2cd2c688a91c35fa2e9ad494d8280ec89a21f
OscarHz <oehz65@gmail.com>
2026-06-04 12:06:06 -0400

    token api laravel

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,30 +1,43 @@
 <?php
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
     function login(Request $request) {
-        $validator = validator()->make($request->all(), ["email" =>
-            'required', 'email',
+        $validator = validator()->make($request->all(), [
+            'email' => 'required|email',
             'password' => 'required'
-        ]);  
+        ]);
         if($validator->fails()){
-            //return $validator->errors();
             return response()->json($validator->errors(),422);
         }
         $credentials = $validator->validated();
        
-        //    if(auth()->attempt($credentials)){
- 
-           if(Auth::attempt($credentials)){
-            session()->regenerate(); // SPA con COOKIE
-            return response()->json('Successful authentication'); // SPA con COOKIE
+        if(Auth::attempt($credentials)){
+            // session()->regenerate(); // SPA con COOKIE
+            // return response()->json('Successful authentication'); // SPA con COOKIE
+            // dd(Auth::user()->createToken('myapptoken'));
+            $user = Auth::user();
+            if (!$user instanceof User) {
+                return response()->json('Invalid credentials',401);
+            }
+            $token = $user->createToken('myapptoken')->plainTextToken;
+            return response()->json($token);
             
         }else{
             return response()->json('Invalid credentials',401);
         }
         
+    }
+    function logout(string $tokenId)  {
+        $user = Auth::user();
+        if (!$user instanceof User) {
+            return response()->json('Unauthenticated', 401);
+        }
+        $user->tokens()->where('id', $tokenId)->delete();
+        return response()->json('Token revoked');
     }
 }
